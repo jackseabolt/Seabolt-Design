7bfd2b7b9868c4e8b456e388acb0977563831544
Jack Seabolt <jackseabolt@gmail.com>
2017-10-25 09:06:32 -0700

    basic layout complete

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+    function dropMobile() {
+        console.log("IT RAN"); 
+        $('html, body').animate({ scrollTop: $("#mobile-first").offset().top}, 'slow');
+    }
+
+    function dropPortfolio() {
+        console.log("IT RAN"); 
+        $('html, body').animate({ scrollTop: $("#portfolio").offset().top}, 'slow');
+    }
+
+    function dropAbout() {
+        console.log("IT RAN"); 
+        $('html, body').animate({ scrollTop: $("#about-me").offset().top}, 'slow');
+    }
+
+    function dropContact() {
+        console.log("IT RAN"); 
+        $('html, body').animate({ scrollTop: $("#contact").offset().top}, 'slow');
+    }
+
+    function parallax(){
+        div = document.getElementById('banner');  
+        ypos = window.pageYOffset; 
+        div.style.top = ypos * 0.5 + 'px'; 
+    }
+    
+    window.addEventListener('scroll', parallax); 
+
+
