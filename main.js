799529bbce55ec11ab092816880acbabaa985ce6
Jack Seabolt <jackseabolt@gmail.com>
2017-11-22 13:53:12 -0800

    email working

--- main.js
+++ main.js
@@ -56,9 +56,41 @@
     }
 
     
+    // FORM CODE
+
+    function handleForm() {
+        $('#js-contact-form').on('submit', event => {
+            console.log("IT FIRED")
+            event.preventDefault(); 
+            return fetch('https://seabolt-design-back.herokuapp.com/contact', {
+                method: 'POST', 
+                body: JSON.stringify({
+                    name: $('#name').val(), 
+                    email: $('#email').val() , 
+                    message: $('#message').val()
+                }), 
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then( res => {
+                if(!res.ok) {
+                    return Promise.reject(res.statusText)
+                }
+                alert("Thank you! I will reply to your message as soon as I am able.")
+                return res.json()
+            })
+            .catch(err => {
+                alert("There was a problem")
+            })
+        }); 
+    }
+
+
     function main() {
         $('#banner').animate({opacity: 1}, 2000);
         window.addEventListener('scroll', parallax); 
+        handleForm(); 
         $('body').on('click', function(event) {
             if(navIsOpen && event.target.id !== 'hamburger') {
                 hideMobileNav(); 
